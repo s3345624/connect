c524bb7d79681cada5949f4e1a77da55adffec35
Ubuntu <ubuntu@ip-172-31-5-158.ap-southeast-2.compute.internal>
2013-09-01 22:21:30 +0000

    result

--- query.php
+++ query.php
@@ -1,5 +1,5 @@
 <?php 
-require_once('connect/connect.php');?>
+require_once('connect.php');?>
 <?php
 mysql_select_db($database_winestore, $winestore);
 $query_Region = "SELECT * FROM region ORDER BY region_name ASC";
